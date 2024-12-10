3ccaaff3a57099dc4c2d0ec90343ee65ed5c96a1
Tamim Ahmed <tamim20072@gmail.com>
2024-12-10 22:21:16 +0600

    Register page done

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className="w-11/12 md:w-10/12 mx-auto">
+            <h2>This is Home</h2>
+        </div>
+    );
+};
+
+export default Home;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./../layout/MainLayout";
+import Home from "../pages/home/Home";
+import Register from "../pages/register/Register";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
-  export default router
+export default router;
