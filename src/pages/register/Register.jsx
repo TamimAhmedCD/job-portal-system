bfad17012ca90efabfece999ce37cce851389372
Tamim Ahmed <tamim20072@gmail.com>
2024-12-10 22:43:33 +0600

    firebase auth done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,24 +1,46 @@
+import Lottie from "lottie-react";
+import registerLottieData from '../../assets/lottie/register.json';
+
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        
+        console.log(email, password);
+
+        if (password.length < 6) {
+            // setError("Password must contain at least 6 characters");
+            return;
+          }
+          if (!/[a-z]/.test(password)) {
+            // setError("Password must have at least one Lowercase letter ");
+            return;
+          }
+          if (!/[A-Z]/.test(password)) {
+            // setError("Password must have at least one Uppercase letter ");
+            return;
+          }
+    }
   return (
     <div className="w-11/12 md:w-10/12 mx-auto">
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+          <div className="text-center lg:text-left w-96">
+            <Lottie animationData={registerLottieData}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-5xl font-bold flex justify-center mt-4">Register now!</h1>
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleRegister}>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
+                  name="email"
                   placeholder="email"
                   className="input input-bordered"
                   required
@@ -30,6 +52,7 @@ const Register = () => {
                 </label>
                 <input
                   type="password"
+                  name="password"
                   placeholder="password"
                   className="input input-bordered"
                   required
